fda800200ffa21f68ec6fc6cc56ab1209d333e4d
github-ci <you@example.com>
2025-04-27 16:52:18 +0000

    忽略无用调用堆栈

--- src/JsonRPCHttpEndpointBundle.php
+++ src/JsonRPCHttpEndpointBundle.php
@@ -3,7 +3,14 @@
 namespace Tourze\JsonRPCHttpEndpointBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Tourze\BacktraceHelper\Backtrace;
+use Tourze\JsonRPCHttpEndpointBundle\Controller\JsonRpcController;
 
 class JsonRPCHttpEndpointBundle extends Bundle
 {
+    public function boot(): void
+    {
+        parent::boot();
+        Backtrace::addProdIgnoreFiles((new \ReflectionClass(JsonRpcController::class))->getFileName());
+    }
 }
