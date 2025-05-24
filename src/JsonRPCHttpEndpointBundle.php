531b989117fadcd60723695b21d79a357452d0dc
github-ci <you@example.com>
2025-05-24 06:48:50 +0000

    update

--- src/JsonRPCHttpEndpointBundle.php
+++ src/JsonRPCHttpEndpointBundle.php
@@ -4,13 +4,21 @@ namespace Tourze\JsonRPCHttpEndpointBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Tourze\BacktraceHelper\Backtrace;
+use Tourze\BundleDependency\BundleDependencyInterface;
 use Tourze\JsonRPCHttpEndpointBundle\Controller\JsonRpcController;
 
-class JsonRPCHttpEndpointBundle extends Bundle
+class JsonRPCHttpEndpointBundle extends Bundle implements BundleDependencyInterface
 {
     public function boot(): void
     {
         parent::boot();
         Backtrace::addProdIgnoreFiles((new \ReflectionClass(JsonRpcController::class))->getFileName());
+    }
+
+    public static function getBundleDependencies(): array
+    {
+        return [
+            \Tourze\JsonRPCEndpointBundle\JsonRPCEndpointBundle::class => ['all' => true],
+        ];
     }
 }
